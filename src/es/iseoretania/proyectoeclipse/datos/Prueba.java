db90bbbfbcfadfbc7f15457919339df998be1f15
Antonio <anaggar2212@g.educaand.es>
2024-10-23 10:13:27 +0200

    Segundo commit

--- src/es/iseoretania/proyectoeclipse/datos/Prueba.java
+++ src/es/iseoretania/proyectoeclipse/datos/Prueba.java
@@ -3,6 +3,7 @@ package es.iseoretania.proyectoeclipse.datos;
 public class Prueba {
 	public static void main (String[]args) {
 		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
 	
 	}
 
